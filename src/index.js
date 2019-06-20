0b815a0b1c834806f90450e88b40ff22e3676cc2
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 23:15:41 -0700

    monolith

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+    <form>
+    <input type="text" onChange={this.props.change}></input>
+    <button onClick={ this.props.click }>Submit</button>
+  </form>
+    );
+  }
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: '',
+    };
+  }
+
+  handleChange = (event) => {
+    const name = event.target.value;
+    this.setState({ name });
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    const count = this.state.count + 1;
+    this.setState({ count });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <div>Name: </div>
-        <div># of Updates: </div>
+        <div>
+          <div>Name: { this.state.name } </div>
+          <div># of Updates: {this.state.count } </div>
+          <Form change={ this.handleChange } click={ this.handleClick } />
+        </div>
       </React.Fragment>
     );
   }
