1ad9ebafbf3d3d9da83e663f69bdc2a4cbe28bed
Morgana Spake <morgana.spake@gmail.com>
2019-06-19 17:20:00 -0700

    starter code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>Name: </div>
+        <div># of Updates: </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
